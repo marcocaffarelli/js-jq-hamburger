148180d4867e9aa79c8491881593e58cc1204ad0
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-26 16:20:02 +0100

    Esercizio completato
    Esercizio completato

--- booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
+++ booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -1,3 +1,21 @@
 //Variabile collegata all'icona hamburger
-var hamburger = $("i > .fa-bars");
+var hamburger = $('.fa-bars');
 //Creare un menu che compare al click dell'icona a forma di hamburger
+hamburger.click(function (){
+    $('.hamburger-menu').fadeIn();
+});
+//Variabile collegata all'icona croce
+var x = $('.fa-times');
+//Far sparire il menu al click dell'icona a forma di X
+x.click(function (){
+    $('.hamburger-menu').fadeOut();
+});
+
+//VERSIONE CON SHOW E HIDE
+// hamburger.click(function (){
+//     $('.hamburger-menu').show();
+// });
+
+// x.click(function (){
+//      $('.hamburger-menu').hide();
+// });
